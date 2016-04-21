e3718ff7601a4b78f805ffb9e3972218773f1494
Cyril Pereira <cyril.pereira@gmail.com>
2016-04-21 23:58:34 +0200

    Removed debug on app.php

--- web/app.php
+++ web/app.php
@@ -6,8 +6,6 @@ $loader = require __DIR__.'/../app/autoload.php';
 
 require_once __DIR__.'/../app/MicroKernel.php';
 
-Debug::enable();
-
 $app = new MicroKernel('prod', false);
 $app->loadClassCache();
 $request = Request::createFromGlobals();
